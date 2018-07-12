4ac8a4ba7fb86216de618c48a4ad2a353afe2d1d
Matt Taylor <mattbtay@gmail.com>
2018-07-12 12:45:29 -0500

    updated email using ref, cool!

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -2,20 +2,25 @@ import React from "react";
 
 class Stage extends React.Component {
 
+    emailRef = React.createRef();
+
     state = {
         modal: 0,
-        email:null
+        email:undefined
     }
 
     showModal = () => {
-        console.log('shown');
-        const emailVal = document.getElementById('emailHolder');
-        console.log(emailVal.value);
+        //const emailVal = document.getElementById('emailHolder');
         this.setState({
             modal: 1,
-            email: emailVal.value
+            email: this.emailRef.value.value
         })
+    }
 
+    hideModal = () => {
+        this.setState({
+            modal:0
+        })
     }
 
     render(){
@@ -28,19 +33,23 @@ class Stage extends React.Component {
                 </header>
 
                 <div className="signup">
-                    <input type="text" id="emailHolder" placeholder="your email here" />
+                    <input type="text" ref={ this.emailRef } placeholder="your email here" />
                     <button id="submitBtn" onClick={this.showModal}>Sign me up</button>
                 </div>
 
+                <div className={`modal-holder ${this.state.modal ? 'modal-holder-active' : ''}`}></div>
                 <div className={`modal ${!this.state.modal ? 'modal-hidden' : ''}`}>
-                    <form action="https://twitter.us18.list-manage.com/subscribe/post" method="POST" _lpchecked="1">
-                        <input type="hidden" name="u" value="92ae1cfac5e13133b0285187a" />
-                        <input type="hidden" name="id" value="7b51010392" />
-                        <input type="text" name="MERGE1" id="MERGE1" size="25"  />
-                        <input type="email"  name="MERGE0" id="MERGE0" value={this.state.email}   />
-                        <ul className="interestgroup_field checkbox-group"> <li className="!margin-bottom--lv2"> <label className="checkbox" htmlFor="gdpr_30905"><input type="checkbox" data-dojo-type="dijit/form/CheckBox" id="gdpr_30905" name="gdpr[30905]" value="Y" className="av-checkbox" /><span>Email</span> </label> </li><li className="!margin-bottom--lv2"> <label className="checkbox" htmlFor="gdpr_30909"><input type="checkbox" data-dojo-type="dijit/form/CheckBox" id="gdpr_30909" name="gdpr[30909]" value="Y" className="av-checkbox" /><span>Direct Mail</span> </label> </li><li className="!margin-bottom--lv2"> <label className="checkbox" htmlFor="gdpr_30913"><input type="checkbox" data-dojo-type="dijit/form/CheckBox" id="gdpr_30913" name="gdpr[30913]" value="Y"  className="av-checkbox" /><span>Customized online advertising</span> </label> </li> </ul>
-                        <input type="submit" className="formEmailButton" name="submit" value="Subscribe to list" />
-                    </form>
+                    <div className="modal-wrap">
+                    <button className="modal-close" onClick={this.hideModal}>X</button>
+                        <form action="https://twitter.us18.list-manage.com/subscribe/post" method="POST" _lpchecked="1">
+                            <input type="hidden" name="u" value="92ae1cfac5e13133b0285187a" />
+                            <input type="hidden" name="id" value="7b51010392" />
+                            <input type="text" name="MERGE1" id="MERGE1" size="25" value={undefined}  />
+                            <input type="email"  name="MERGE0" id="MERGE0" value={this.state.email ? this.state.email : undefined}   />
+                            <ul className="interestgroup_field checkbox-group"> <li className="!margin-bottom--lv2"> <label className="checkbox" htmlFor="gdpr_30905"><input type="checkbox" data-dojo-type="dijit/form/CheckBox" id="gdpr_30905" name="gdpr[30905]" value="Y" className="av-checkbox" /><span>Email</span> </label> </li><li className="!margin-bottom--lv2"> <label className="checkbox" htmlFor="gdpr_30909"><input type="checkbox" data-dojo-type="dijit/form/CheckBox" id="gdpr_30909" name="gdpr[30909]" value="Y" className="av-checkbox" /><span>Direct Mail</span> </label> </li><li className="!margin-bottom--lv2"> <label className="checkbox" htmlFor="gdpr_30913"><input type="checkbox" data-dojo-type="dijit/form/CheckBox" id="gdpr_30913" name="gdpr[30913]" value="Y"  className="av-checkbox" /><span>Customized online advertising</span> </label> </li> </ul>
+                            <input type="submit" className="formEmailButton" name="submit" value="Subscribe to list" />
+                        </form>
+                    </div>
                 </div>
 
             </div>
