9c78af1531574b989af5d35c2c8127c155dc5978
Matt Taylor <mattbtay@gmail.com>
2018-07-12 08:55:08 -0500

    state updates for email

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -2,17 +2,23 @@ import React from "react";
 
 class Stage extends React.Component {
 
+    state = {
+        modal: 0,
+        email:null
+    }
 
     showModal = () => {
         console.log('shown');
         const emailVal = document.getElementById('emailHolder');
         console.log(emailVal.value);
-        let modalStatus = 1;
+        this.setState({
+            modal: 1,
+            email: emailVal.value
+        })
+
     }
 
     render(){
-
-        let modalStatus = 0;
 
         return (
             <div className="stage">
@@ -26,12 +32,12 @@ class Stage extends React.Component {
                     <button id="submitBtn" onClick={this.showModal}>Sign me up</button>
                 </div>
 
-                <div className={`modal modal-hidden`}>
+                <div className={`modal ${!this.state.modal ? 'modal-hidden' : ''}`}>
                     <form action="https://twitter.us18.list-manage.com/subscribe/post" method="POST" _lpchecked="1">
                         <input type="hidden" name="u" value="92ae1cfac5e13133b0285187a" />
                         <input type="hidden" name="id" value="7b51010392" />
                         <input type="text" name="MERGE1" id="MERGE1" size="25"  />
-                        <input type="email"  name="MERGE0" id="MERGE0"   />
+                        <input type="email"  name="MERGE0" id="MERGE0" value={this.state.email}   />
                         <ul className="interestgroup_field checkbox-group"> <li className="!margin-bottom--lv2"> <label className="checkbox" htmlFor="gdpr_30905"><input type="checkbox" data-dojo-type="dijit/form/CheckBox" id="gdpr_30905" name="gdpr[30905]" value="Y" className="av-checkbox" /><span>Email</span> </label> </li><li className="!margin-bottom--lv2"> <label className="checkbox" htmlFor="gdpr_30909"><input type="checkbox" data-dojo-type="dijit/form/CheckBox" id="gdpr_30909" name="gdpr[30909]" value="Y" className="av-checkbox" /><span>Direct Mail</span> </label> </li><li className="!margin-bottom--lv2"> <label className="checkbox" htmlFor="gdpr_30913"><input type="checkbox" data-dojo-type="dijit/form/CheckBox" id="gdpr_30913" name="gdpr[30913]" value="Y"  className="av-checkbox" /><span>Customized online advertising</span> </label> </li> </ul>
                         <input type="submit" className="formEmailButton" name="submit" value="Subscribe to list" />
                     </form>
