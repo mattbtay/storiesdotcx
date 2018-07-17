1c46eae3697b5b7b2e7766ab20ab48a3c38828a1
Matt Taylor <mattbtay@gmail.com>
2018-07-16 21:45:03 -0500

    gh forms

--- src/components/SendStory.js
+++ src/components/SendStory.js
@@ -5,15 +5,22 @@ class SendStory extends React.Component {
     render(){
         return (
             <div className="section section--sendstory">
-                <div>
-                    <form name="story-submissions" method="POST" netlify>
-                        <input type="hidden" name="form-name" value="story-submissions" />
-                        <input name="name" type="text" />
-                        <input name="email" type="email" />
 
+                <h2 className="recent-title">send us your story</h2>
+
+                <div className="storyForm--wrap">
+                    <form name="story-submissions" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
                         <div>
-                            <input name="brand" type="text" />
-                            <textarea name="story" />
+                            <textarea name="Story" />
+                        </div>
+                        <div>
+                            <input type="hidden" name="form-name" value="story-submissions" />
+                            <input name="Name" type="text" autoComplete="off" />
+                            <input name="Email Address" type="email" />
+                        </div>
+                        <div>
+                            <input name="Company" type="text" />
+
                         </div>
                         <button type="submit">send</button>
                     </form>
