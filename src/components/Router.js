55a7537f4456e3bac424ca9d068231bf2fb82cbb
Matt Taylor <mattbtay@gmail.com>
2018-07-20 20:08:35 -0500

    update build

--- src/components/Router.js
+++ src/components/Router.js
@@ -7,7 +7,7 @@ const Router = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={App} />
-            <Route exact path="/confirmation" component={Confirmation} />
+            <Route path="/confirmation" component={Confirmation} />
             <Route component={App} />
         </Switch>
     </BrowserRouter>
