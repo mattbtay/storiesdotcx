2a8dcb92b137d9e6644404530c3a8c344a192ce8
Matt Taylor <mattbtay@gmail.com>
2018-07-20 19:57:08 -0500

    update confirmation

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Stage from "./Stage";
 import RecentStories from "./RecentStories";
 import SendStory from "./SendStory";
 import Footer from "./Footer";
@@ -9,7 +8,6 @@ class Confirmation extends React.Component {
     render(){
         return (
             <React.Fragment>
-                <Stage />
                 <RecentStories />
                 <SendStory />
                 <Footer />
