17c8a49b0a3cc3a0fe6b81ac27e388c5faa9a0ea
Matt Taylor <mattbtay@gmail.com>
2018-07-20 21:24:43 -0500

    using hash url

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 import Confirmation from "./Confirmation";
 
 const Router = () => (
-    <BrowserRouter>
+    <HashRouter>
         <Switch>
             <Route exact path="/" component={App} />
-            <Route path="/confirmation/:pageid" component={Confirmation} />
+            <Route path="/confirmation" component={Confirmation} />
             <Route component={App} />
         </Switch>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 
